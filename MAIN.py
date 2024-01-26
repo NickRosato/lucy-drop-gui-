0ed0f9cbb49c27ccb1876cab4d890d4bd6198943
Nick Rosato <nrosato111@gmail.com>
2024-01-26 16:26:25 -0500

    Exit Button added

--- MAIN.py
+++ MAIN.py
@@ -3,14 +3,12 @@
 import tkinter as tk
 from tkinter import ttk
 
-# -------------------------- DEFINING GLOBAL VARIABLES -------------------------
 
 selectionbar_color = 'white'
 sidebar_color = 'white'
 header_color = '#E61231'
 visualisation_frame_color = "#ffffff"
 frame_color = "#808080"
-# ------------------------------- ROOT WINDOW ----------------------------------
 
 #Shape Builder
 p_x = 0.15
@@ -22,9 +20,6 @@ class TkinterApp(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.title("Lucy Drop Tower")
-
-        # ------------- BASIC APP LAYOUT -----------------
-
         self.geometry("1600x1000")
         self.attributes('-fullscreen', True)
         #self.bind("<F11>", lambda event: self.attributes("-fullscreen", not self.attributes("-fullscreen")))
@@ -36,17 +31,16 @@ class TkinterApp(tk.Tk):
         icon = tk.PhotoImage(file= filepath +'info.png')
         self.iconphoto(True, icon)
 
-        # ---------------- HEADER ------------------------
+
 
         self.header = tk.Frame(self, bg=header_color)
         self.header.place(relx=p_x, rely=0, relwidth=1-p_x, relheight=p_y)
 
-        # ---------------- SIDEBAR -----------------------
-        # CREATING FRAME FOR SIDEBAR
+
         self.sidebar = tk.Frame(self, bg=sidebar_color)
         self.sidebar.place(relx=0, rely=0, relwidth=p_x, relheight=1)
 
-        # LOGO AND NAME
+
         self.brand_frame = tk.Frame(self.sidebar, bg=sidebar_color, highlightthickness=1)
         self.brand_frame.place(relx=0, rely=0, relwidth=1, relheight=p_y)
         self.uni_logo = icon.subsample(5)
@@ -55,27 +49,12 @@ class TkinterApp(tk.Tk):
         name = tk.Label(self.brand_frame,text='Lucy Drop Tower',bg=sidebar_color,font=("", 15, "bold"))
         name.place(relx=.25, rely=.5, anchor="w")
 
-        # SUBMENUS IN SIDE BAR
-
-        # # SUBMENU 1
         
-        # --------------------  MULTI PAGE SETTINGS ----------------------------
-
-        container = tk.Frame(self)
-        container.config(highlightbackground=frame_color, highlightthickness=1)
-        container.place(relx=p_x, rely=p_y, relwidth=1-p_x, relheight=1-p_y)
+        main = tk.Frame(self)
+        main.config(highlightbackground=frame_color, highlightthickness=1)
+        main.place(relx=p_x, rely=p_y, relwidth=1-p_x, relheight=1-p_y)
         
 
-# ------------------------ MULTIPAGE FRAMES ------------------------------------
-
-
-class Frame1(tk.Frame):
-    def __init__(self, parent, controller):
-
-        tk.Frame.__init__(self, parent)
-
-        label = tk.Label(self, text='Frame 1', font=("Arial", 15))
-        label.pack()
 
 
 
