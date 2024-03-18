f67ba66d9b772e0d04d7f98d3422c47ec4369d7b
NickRosato <nrosato111@gmail.com>
2024-03-18 17:33:13 -0400

    Issue is - to float

--- Ard.py
+++ Ard.py
@@ -3,11 +3,12 @@ import time
 # set up the serial line
 ser = serial.Serial('COM4', 115200)
 time.sleep(2)
+
 # Read and record the data
 data =[]                       # empty list to store the data
 for i in range(50):
 	b = ser.readline()         # read a byte string
-    string_n = b.decode()  # decode byte string into Unicode  
+	string_n = b.decode()  # decode byte string into Unicode  
 	string = string_n.rstrip() # remove \n and \r
 	flt = float(string)        # convert string to float
 	print(flt)
@@ -15,3 +16,4 @@ for i in range(50):
 	time.sleep(0.1)            # wait (sleep) 0.1 seconds
 
 ser.close()
+
